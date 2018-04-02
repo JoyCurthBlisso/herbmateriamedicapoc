b95daef44e59bc86de16209f910cebb3bd8f3673
Joy Curth <joy.curth@comcast.net>
2018-04-02 14:53:07 -0400

    wip

---
+++ controllers/herbController.js
@@ -0,0 +1,38 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+var herb = require('../models/herb.js');
+
+router.get('/', function(req, res) {
+  herb.selectAll(function(data) {
+    var hbsObject = {
+      herb: data
+    };
+  
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/herb', function(req, res) {
+  herb.insertOne([
+    'herb_name'
+  ], [
+    req.body.herb_name
+  ], function(data) {
+    res.redirect('/');
+  });
+});
+
+router.put('/herb/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  herb.updateOne({
+    picked: true
+  }, condition, function(data) {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
